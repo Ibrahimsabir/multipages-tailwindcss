313ef3a4cef07b54219d42eb063c5979184c133f
Saleha Khan <161796561+Ibrahimsabir@users.noreply.github.com>
2024-11-18 16:05:36 -0800

    made changes

--- src/app/about/aboutnested/page.tsx
+++ src/app/about/aboutnested/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import Image from "next/image";
 import Typewriter from 'typewriter-effect';
 // import Hero from "./components/hero";
 
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,11 @@
 "use client"
-import Image from "next/image";
 import Typewriter from 'typewriter-effect';
-import styles from "./about.module.css";
 // import Hero from "./components/hero";
 
 export default function About() {
   return (
     <div className="flex flex-col items-center mt-32">
-    <h2 className={styles.gradient} >
-    Welcome To The About Page...
-      </h2>
+  
       <h1 className="tracking-widest text-2xl title-font font-medium text-blue-400 mb-1">
         
         <br className="hidden lg:inline-block" />
